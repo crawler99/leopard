9bb9ff9198ee2d181af4b512412e678b231b919e
crawler99 <taojiang99@gmail.com>
2017-11-27 15:09:21 +0800

    small change

--- utils/Event.hxx
+++ utils/Event.hxx
@@ -2,35 +2,52 @@
 #define LEOPARD_UTILS_EVENT_HXX_
 
 #include "NonCopyable.hxx"
+#include <mutex>
 
 namespace leopard { namespace utils {
 
 //
 // - Thread safe.
-// - Create instance with arbitrary arguments (perfect-forwarding).
+// - Attach observers with arbitrary arguments (perfect-forwarding).
 //
 template <typename Func>
 class Event : private NonCopyable
 {
 public:
+    template <typename F>
+    uint32_t Connect(F&& f)
+    {
+        std::lock_guard<std::mutex> guard(_mutex);
+        uint32_t k = _observer_id++;
+        _connections.emplace(k, std::forward<F>(f));
+        return k;
+    }
+    
+    
+    /*
     uint32_t Connect(Func&& f)
     {
+        std::lock_guard<std::mutex> guard(_mutex);
         return Assign(f);
     }
 
     uint32_t Connect(const Func &f)
     {
+        std::lock_guard<std::mutex> guard(_mutex);
         return Assign(f);
     }
+    */
 
     void Disconnect(uint32_t key)
     {
+        std::lock_guard<std::mutex> guard(_mutex);
         _connections.erase(key);
     }
 
     template <typename... Args>
     void Notify(Args&&... args)
     {
+        std::lock_guard<std::mutex> guard(_mutex);
         for (auto &it : _connections)
         {
             it.second(std::forward<Args>(args)...);
@@ -38,14 +55,9 @@ public:
     }
 
 private:
-    template <typename F>
-    uint32_t Assign(F&& f)
-    {
-        uint32_t k = _observer_id++;
-        _connections.emplace(k, std::forward<F>(f));
-        return k;
-    }
+    
 
+    std::mutex _mutex;
     uint32_t _observer_id = { 0 };
     std::map<uint32_t, Func> _connections;
 };
